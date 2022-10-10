70d2664768cf31a383d4319f68dd7803197e29fd
ZangZehua <zhzang97@gamil.com>
2022-10-11 07:43:25 +0800

    fix

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import shutil
 import random
 
 
-class_number = 10
-pick_number_each_class = 500
-data_folder = "e:/data/CIFAR-10/unlabeled"
-target_folder = "e:/data/CIFAR-10/train"
+class_number = 100
+image_number_each_class = 500
+pick_rate = 0.1
+data_folder = "/data/data/CIFAR-100/unlabeled"
+target_folder = "/data/data/CIFAR-100/train"
 if not os.path.exists(target_folder):
     os.mkdir(target_folder)
 class_file_list = os.listdir(data_folder)
@@ -21,7 +22,7 @@ for c in range(class_number):
     print(current_path)
     print(target_path)
     image_file_list = os.listdir(current_path)
-    selected_image_path = random.sample(range(0, 5000), pick_number_each_class)
+    selected_image_path = random.sample(range(0, image_number_each_class), int(pick_rate*image_number_each_class))
     for image in selected_image_path:
         image_path = os.path.join(current_path, image_file_list[image])
         target_image_path = os.path.join(target_path, image_file_list[image])
